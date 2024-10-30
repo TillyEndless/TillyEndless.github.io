4e21429b5a3dd2535c2ef9a41771eb6d90e0ec03
TillyEndless <3095570745@qq.com@https://github.com/TillyEndless>
2024-10-30 18:07:01 +0800

    Deploy new post.

--- src/utils/getHeadings.js
+++ src/utils/getHeadings.js
@@ -1,9 +1,24 @@
 import { remark } from 'remark';
 import remarkHeadingId from 'remark-heading-id';
+import remarkHtml from 'remark-html'; // 导入 remark-html 库
 import { visit } from 'unist-util-visit';
 
-export async function getHeadings(markdown) {
+// 将 HTML 转换为 Markdown
+async function convertHtmlToMarkdown(html) {
+  const markdown = await remark()
+    .use(remarkHtml)
+    .process(html);
+  
+  return markdown.toString();
+}
+
+export async function getHeadings(content) {
   const headings = [];
+
+  // 确保 content 是字符串并判断内容是 HTML 格式还是 Markdown 格式
+  const markdownContent = (typeof content === 'string' && content.startsWith('<')) 
+    ? await convertHtmlToMarkdown(content) 
+    : content;
 
   await remark()
     .use(remarkHeadingId)
@@ -16,7 +31,7 @@ export async function getHeadings(markdown) {
         });
       });
     })
-    .process(markdown);
+    .process(markdownContent);
 
   return headings;
 }
